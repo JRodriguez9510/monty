366e7d620b0525638d7f4ee7d6da1a2f666a3f6f
JRodriguez9510 <jrodriguez9510@outlook.com>
2020-12-22 22:48:00 -0500

    Update library

--- monty.h
+++ monty.h
@@ -1,4 +1,4 @@
-#ifndef _MONTY_H_ 
+#ifndef _MONTY_H_
 #define _MONTY_H_
 
 #include <stdio.h>
@@ -10,6 +10,12 @@
 #include <limits.h>
 #include <string.h>
 #include <ctype.h>
+
+#define TRUE (1 == 1)
+#define FALSE (!TRUE)
+#define BUFSIZE 1024
+#define LIFO 1
+#define FIFO 0
 
 #define ERROR_MALLOC 0
 #define ERROR_USAGE_FILE 1
@@ -28,12 +34,6 @@
 #define ERROR_STACK_EMPTY 14
 #define ERROR_PCHAR_RANGE 15
 
-#define TRUE (1 == 1)
-#define FALSE (!TRUE)
-#define BUFSIZE 1024
-#define LIFO 1
-#define FIFO 0
-
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -41,27 +41,27 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO Bootcamp project
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
- * struct instruction_s - opcode and its function
+ * struct instruction_s - opcoode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO Bootcamp project
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /**
@@ -90,12 +90,31 @@ inventory_t *inventory;
 
 int build_inventory(void);
 
+void add_node_lifo(stack_t **stack, stack_t *new_node);
+void add_node_fifo(stack_t **stack, stack_t *new_node);
+void _push(stack_t **stack, unsigned int line_number);
+
 void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
 void _swap(stack_t **stack, unsigned int line_number);
+
 void _add(stack_t **stack, unsigned int line_number);
 void _nop(stack_t **stack, unsigned int line_number);
+void _sub(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+
+void _mod(stack_t **stack, unsigned int line_number);
+void _pchar(stack_t **stack, unsigned int line_number);
+void _pstr(stack_t **stack, unsigned int line_number);
+void _rotl(stack_t **stack, unsigned int line_number);
+void _rotr(stack_t **stack, unsigned int line_number);
+
+void _stack(stack_t **stack, unsigned int line_number);
+void _queue(stack_t **stack, unsigned int line_number);
+
+int are_digits(char *num);
 
 void (*match_opcode(void))(stack_t **stack, unsigned int line_number);
 int parse_line(char *line);
@@ -103,9 +122,5 @@ int parse_line(char *line);
 void free_all(void);
 void free_stack(void);
 void handle_errors(int e);
-
-void add_node_lifo(stack_t **stack, stack_t *new_node);
-void add_node_fifo(stack_t **stack, stack_t *new_node);
-void _push(stack_t **stack, unsigned int line_number);
 
 #endif
